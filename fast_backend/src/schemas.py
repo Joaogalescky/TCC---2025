ef9bc284f07f7d252c0cc209392e4e91d6168e52
Joaogalescky <joaocvgalescky@gmail.com>
2025-08-26 17:07:07 -0300

    feat: class UserSchema

--- fast_backend/src/schemas.py
+++ fast_backend/src/schemas.py
@@ -1,4 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class Message(BaseModel):
     message: str
+    
+class UserSchema(BaseModel):
+    nome: str
+    cpf: str
+    telefone: str
+    email: EmailStr
+    password: str
+    tipo: str
+    statusVotacao: bool
