5693fe60ea00db9d65538836b3aca2dd4bec7657
Joaogalescky <joaocvgalescky@gmail.com>
2025-09-27 21:15:19 -0300

    adicionada a primeira migração com Alembic. Criada tabela de usuários

--- fast_backend/tests/conftest.py
+++ fast_backend/tests/conftest.py
@@ -1,9 +1,61 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from src.app import app
+from src.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    """
+    Configura e limpa um banco de dados de teste
+    para cada teste que o solicita,
+    assegurando que cada teste seja isolado
+    e tenha seu próprio ambiente limpo para trabalhar.
+    """
+    engine = create_engine('sqlite:///:memory:')  # cria uma sessão em memória
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:  # cria uma sessão para os teste
+        yield session  # fornece uma instância para teste
+
+    table_registry.metadata.drop_all(engine)  # apaga as tabelas
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 1, 1)):
+    """
+    Gerenciador de contexto.
+    Toda vezes que um registro de model for inserido no banco de dados,
+    se ele tiver o campo created_at, por padrão, o campo será cadastrado
+    com a sua data pré-fixada.
+    Facilita a manutenção dos testes que precisam da comparação de data,
+    pois será determinística.
+    """
+
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
---
+++ fast_backend/tests/test_db.py
@@ -0,0 +1,36 @@
+from dataclasses import asdict
+
+from sqlalchemy import Select
+
+from src.models import User
+
+
+def test_create_user(session, mock_db_time):
+    """
+    Adiciona um novo usuário ao banco de dados,
+    faz commit das mudanças, e depois verifica se o usuário
+    foi devidamente criado consultando-o pelo nome de usuário.
+    Se o usuário foi criado corretamente, o teste passa.
+    Caso contrário, o teste falha, indicando que há algo errado
+    com nossa função de criação de usuário
+    """
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='teste', password='teste', email='teste@email.com'
+        )
+        session.add(new_user)  # adiciona o registro a sessão
+        session.commit()  # realiza a inserção ao banco
+
+    # busca o dado
+    user = session.scalar(Select(User).where(User.username == 'teste'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'teste',
+        'password': 'teste',
+        'email': 'teste@email.com',
+        'created_at': time,
+        'updated_at': time,
+    }
+
+    assert user.username == 'teste'
