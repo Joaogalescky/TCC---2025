04c72dc08140894b962d4d7d4b14e40309aacf17
Joaogalescky <joaocvgalescky@gmail.com>
2025-11-20 23:21:09 -0300

    refactor: refazendo estrutura inicial do frontend com SvelteKit - seguindo um tutorial

--- svelte_frontend/src/routes/+page.svelte
+++ svelte_frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+    let name = "World";
+</script>
+
+<h1>Hello, {name}!</h1>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
