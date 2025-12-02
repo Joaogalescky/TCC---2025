5ae66eb42f76e5c043848624503831b8bf378a60
Joaogalescky <joaocvgalescky@gmail.com>
2025-12-02 00:55:06 -0300

    fix: inserido CORS para integrar backend e frontend

--- fast_backend/src/app.py
+++ fast_backend/src/app.py
@@ -1,12 +1,20 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import auth, candidates, elections, users, vote
 from src.schemas import Message
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
